341a067f618aa64e6a9ee72b34181e9e54b331f8
runchentao <runchentao@gmail.com>
2020-12-06 16:19:59 -0800

    first commit

---
+++ dim-sum.php
@@ -0,0 +1,4 @@
+<!-- stylesheets css -->
+<a href="#" id="dim-sum" onclick="document.location=this.id+'.php';return false;">
+    <img src="images/dim sum.jpeg" class="img-responsive" alt="Food Menu" />
+</a>
