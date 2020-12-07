cc1da64d9b8d36450bc75a9d56cf3eb499f283a2
runchentao <runchentao@gmail.com>
2020-12-06 22:36:15 -0800

    added product pages

--- dim-sum.php
+++ dim-sum.php
@@ -1,4 +1,1 @@
-<!-- stylesheets css -->
-<a href="#" id="dim-sum" onclick="document.location=this.id+'.php';return false;">
-    <img src="images/dim sum.jpeg" class="img-responsive" alt="Food Menu" />
-</a>
+<img src="images/dim sum.jpeg" class="img-responsive" alt="Food Menu" />
